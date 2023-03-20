f34a60bf959aedd5b3f9fdc40c529b5d2077d8a9
Pavani Vidiyala <vidiyalapavanichowdary@gmail.com>
2023-03-20 20:01:09 +0530

    Autopush

--- 03-colors.sh
+++ 03-colors.sh
@@ -18,3 +18,9 @@
  # Ex:  
  #      echo -e "\e[COL-CODEm  Your Message To Be Printed \e[0m"
  #      echo -e "\e[32m I am printing Green Color \e[0m"
+
+ echo -e "\e[32m I am printing Green Color \e[0m"
+echo -e "\e[31m I am printing Red Color \e[0m"
+echo -e "\e[33m I am printing Yellow Color \e[0m"
+echo -e "\e[34m I am printing Blue Color \e[0m"
+
