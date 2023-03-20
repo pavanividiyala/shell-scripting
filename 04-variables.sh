7ab8742a64810740e0505b6b4ebde1a005f08515
Pavani Vidiyala <vidiyalapavanichowdary@gmail.com>
2023-03-20 20:14:37 +0530

    Autopush

--- 04-variables.sh
+++ 04-variables.sh
@@ -16,3 +16,23 @@ c=30
 echo $a 
 echo ${a} 
 echo "$c"
+
+# No data types are available in Bash by defailt 
+# Everything is considered as a STRING.
+
+# How do you print a variable ?
+# We use a special character, called $ to print 
+# echo $a 
+
+echo $a 
+echo ${a} 
+echo "$c"
+
+echo "I am pritning the value of d $d" 
+
+# When you try to print a variable which is not declared, bash is going to consider that as Null or empty
+
+# rm -rf /data/${DATA_DIR}   # /data/test  ---> rm -rf /data/
+
+# How do you supply variables from the command line 
+# export varName =  value 
