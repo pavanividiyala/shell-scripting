5ca90058c7baca61a656f5a02ce21d4152d9da8e
Pavani Vidiyala <vidiyalapavanichowdary@gmail.com>
2023-03-20 21:23:24 +0530

    Autopush

--- 09-functions.sh
+++ 09-functions.sh
@@ -8,3 +8,22 @@
 # 4) Functions                # Functions are nothing but a set of command that can be written in a sequence and can be called n number of times as per your choice.
 
 # Declaring a function 
+
+
+sample() {
+    echo "I am a messaged called from sample function"
+}
+
+# This is how we call a function.
+ stat() {
+    echo -e "\t Total number of sessons : $(who | wc -l)"
+    echo "Todays date is $(date +%F)"
+    echo "Load Average On The system is $(uptime | awk -F : '{print $NF}' | awk -F , '{print $1}')"
+    echo -e "\t stat function completed"
+
+    echo "Calling sample function"
+    sample 
+ }
+
+ echo "calling stat function" 
+ stat
