246ad5a5b841846a72947de16a255e7be62d306a
Pavani Vidiyala <vidiyalapavanichowdary@gmail.com>
2023-03-24 13:07:43 +0530

    Autopush

---
+++ 10-exit-code.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+
+# Each and every action will have an EXIT Code in Linux, which helps us in determining the status of the task
+# Exit Code ranges from 0 to 255 
+ 
+ # While 0 is Success ; rest of them may be 1) partial failure 2) complete failure 3) partial success
+
+# How to fetch the exit status of the previous executed command ?
+# --> $? : This is going to print the exit code of the previous executed command only .
